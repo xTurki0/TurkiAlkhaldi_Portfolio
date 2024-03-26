ad2c69230fcba8b942f799e217ba72325d2354db
xTurki0 <162879384+xTurki0@users.noreply.github.com>
2024-03-26 03:54:26 +0000

    New contactacus

--- app.py
+++ app.py
@@ -1,30 +1,13 @@
-from database import load_projects_from_db, load_project_from_db
+from flask import Flask,render_template
+from database import load_projects_from_db
 
-app = Flask(_name_)
-
+app = Flask(__name__)
 
 @app.route("/")
+
 def hello_world():
-  projects = load_projects_from_db()
-  return render_template('home.html', projects=projects)
+    projects=load_projects_from_db()
+    return render_template("home.html",projects=projects)
 
-
-@app.route("/project/<id>")
-def project_details(id):
-  project = load_project_from_db(id)
-  return render_template('project_details.html', project=project)
-
-
-@app.route("/message")
-def message():
-  return render_template('contactus.html')
-
-
-@app.route("/message_submitted", methods=['post'])
-def message_submitted():
-  data = request.form
-  return render_template('message_submitted.html', data=data)
-
-
-if _name_ == '_main_':
-  app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',debug=True)
