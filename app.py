ec68607e03b5f842fb8e61e32b566c6c1e8922e8
xTurki0 <162879384+xTurki0@users.noreply.github.com>
2024-03-27 23:31:59 +0000

    .

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-@app.route('/message')
-def message():
+from flask import Flask,render_template,request
+from database import add_message_to_db, load_projects_from_db,load_project_from_db
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def hello_world():
+    projects=load_projects_from_db()
+    return render_template("home.html",projects=projects)
+
+@app.route("/project/<id>")
+def project_details(id):
+  project=load_project_from_db(id)
+  return render_template("project_details.html",project=project)
+
+@app.route("/massage")
+def massage():
   return render_template('contactus.html')
 
 
-@app.route("/message_submitted", methods=['POST'])
+@app.route("/message_submitted",methods=['POST'])
 def message_submitted():
   data = request.form
   add_message_to_db(data)
-  return render_template('message_submitted.html', data=data)
+  return render_template('message_submitted.html',data=data)
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -11,3 +11,17 @@ def load_projects_from_db():
     for row in result.all():
       projects.append(dict(row._mapping))
   return projects
+
+def load_project_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from Myprojects where id = {id}"))
+    rows = result.all()
+    return dict(rows[0]._mapping)
+
+def add_message_to_db(data):
+  with engine.connect() as conn:
+    sql = text(
+        f"INSERT INTO mymessages (Full_name, email, message) VALUES (\'{data['full_name']}\', \'{data['email']}\', \'{data['message']}\')"
+    )
+    conn.execute(sql)
+    conn.commit()
